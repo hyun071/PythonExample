4d6f5d11f2eb96cd10b4653684463eaa94660807
hyun071 <rhdclsla@gmail.com>
2023-02-07 22:36:27 +0900

    파이썬기본예제

---
+++ app.py
@@ -0,0 +1,25 @@
+# car = ['k5' , 'white' ,5000 ]
+# car[1] = 'black'
+# car2 = {'brand' : 'BMW' , 'model' : '520d'}
+# car2['brand'] = 123
+# print(car2['brand'])
+
+
+# 중고차재고 = ['k5', 'bmw', 'Tico']
+# if  'k6' in 중고차재고 :
+#     print('지금 주문 가능합니다!')
+# else : print('주문 불가능합니다')
+
+
+# 중고차들 = ['k5', 'bmw', 'Tico']
+# for i in 중고차들 :
+#     print(i * 3)
+
+
+def 모자(구멍, 구멍2) :
+    print(구멍 + 구멍2)
+모자(1,2)
+
+def 함수() :
+    return 10
+print(함수())
